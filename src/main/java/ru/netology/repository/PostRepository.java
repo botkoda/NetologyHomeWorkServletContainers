89ffcf2fec863b67d6c4d1bb4466bda317aa02c1
botkoda <botkoda@gmail.com>
2021-12-19 21:22:15 +0400

    feature/di-java

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,16 +1,12 @@
 package ru.netology.repository;
 
-import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
-@Repository
 public class PostRepository {
     List<Post> postList = new CopyOnWriteArrayList<>();
     AtomicInteger postNumber = new AtomicInteger();
