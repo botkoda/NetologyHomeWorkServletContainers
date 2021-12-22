fe069798ef998db1385cbb307a03ca28445575c4
botkoda <botkoda@gmail.com>
2021-12-22 19:35:53 +0400

    ConcurrentHashMap

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,39 +2,41 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-    List<Post> postList = new CopyOnWriteArrayList<>();
-    AtomicInteger postNumber = new AtomicInteger();
+    Map<Long, Post> postMap = new ConcurrentHashMap();
+    AtomicLong postNumber = new AtomicLong();
 
     public List<Post> all() {
+        List<Post> postList = new ArrayList<>();
+        postMap.forEach((k, v) -> postList.add(v));
         return postList;
     }
 
     public Optional<Post> getById(long id) {
-        var post = postList.stream().filter(x -> x.getId() == id).findAny();
-        return post;
+        //  var post = postList.stream().filter(x -> x.getId() == id).findAny();
+        return Optional.ofNullable(postMap.get(id));
     }
 
     public Post save(Post post) {
         if (post.getId() == 0) {
             post.setId(postNumber.addAndGet(1));
-            postList.add(post);
+            postMap.put(postNumber.get(), post);
+            //  postList.add(post);
         } else {
-            postList.set(postList.indexOf(post), post);
+            postMap.put(post.getId(), post);
+            // postList.set(postList.indexOf(post), post);
         }
         return post;
     }
 
     public void removeById(long id) {
-        var post = postList.stream().filter(x -> x.getId() == id).findAny();
-        postList.remove(post);
+        postMap.remove(id);
     }
 }
